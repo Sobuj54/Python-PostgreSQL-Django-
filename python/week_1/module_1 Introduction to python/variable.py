21ea615d86c1cdb49867ac2e8efc2a49f2c3a857
soubj <sobujahmed124@gmail.com>
2025-03-23 16:57:46 +0600

    python variables and f string

---
+++ python/week_1/module_1 Introduction to python/variable.py
@@ -0,0 +1,18 @@
+age = 24
+taka = 100.1
+name = "Sobuj Ahmed"
+isSingle = True
+isDead = False
+
+print(age + taka)
+print(name)
+print(isSingle)
+
+# type checking
+print(type(taka))
+print(type(name))
+print(type(isSingle))
+
+# f string injects variables in string
+text = f"sobuj er boyosh {age}"
+print(text)
