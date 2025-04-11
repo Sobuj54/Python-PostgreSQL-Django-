f69edd87694046ea67482bbcaa44d825c30a30ff
soubj <sobujahmed124@gmail.com>
2025-04-11 21:33:53 +0600

    add person module with Person, Teacher, and Student classes; implement exam evaluation and final grade calculation

---
+++ python/week_3/module_10 school management system/person.py
@@ -0,0 +1,35 @@
+import random
+from school import School
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Teacher(Person):
+    def __init__(self, name):
+        super().__init__(name)
+    def evaluate_exam(self):
+        return random.randint(1,100)
+    
+class Student(Person):
+    def __init__(self, name, classroom):
+        super().__init__(name)
+        self.classroom = classroom
+        self.__id = None
+        self.marks = {}  # { "english" : 80 }
+        self.subject_grade = {}  # { "english" : "A+" }
+        self.grade = None # final grade
+
+    def final_grade(self):
+        total = 0
+        for grade in self.subject_grade.values():
+            total += School.grade_to_value(grade)
+        gpa = total / len(self.subject_grade)
+        self.grade = School.value_to_grade(gpa)
+
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, num):
+        self.__id = num
