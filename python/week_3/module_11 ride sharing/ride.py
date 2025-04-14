5a78419ac581368ac3a106a882667598c2397724
soubj <sobujahmed124@gmail.com>
2025-04-14 22:44:46 +0600

    add ride and user management classes for ride sharing system; implement ride requests and driver acceptance

---
+++ python/week_3/module_11 ride sharing/ride.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+class Ride:
+    def __init__(self, start_location, end_location):
+        self.start_location = start_location
+        self.end_location = end_location
+        self.driver = None
+        self.rider = None
+        self.start_time = None
+        self.end_time = None
+        self.estimated_fare = None
+
+    def set_driver(self, driver):
+        self.driver = driver
+    
+    def start_ride(self):
+        self.start_time = datetime.now()
+    
+    def end_ride(self):
+        self.end_time = datetime.now()
+        self.rider.wallet -= self.estimated_fare
+        self.driver.wallet += self.estimated_fare
+    
+    def __repr__(self):
+        return f"ride started from {self.start_location} to {self.end_location}"
+    
+class RideRequest:
+    def __init__(self, rider, end_location):
+        self.rider = rider
+        self.end_location = end_location
+
+class RideMatching:
+    def __init__(self, drivers):
+        self.available_drivers = drivers
+    
+    def find_driver(self, ride_request):
+        if len(self.available_drivers):
+            print("drivers: ")
+            driver = self.available_drivers[0]
+            ride = Ride(ride_request.rider.current_location, ride_request.end_location)
+            driver.accept_ride(ride)
+            return ride
+    
+    
