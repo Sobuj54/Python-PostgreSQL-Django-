0a72dad1c12ab2fcc2cc56007e74cd68677cbd1a
sobuj54 <sobujahmed124@gmail.com>
2025-07-08 21:19:48 +0600

    ready for render deployment

--- milestone 1 getting started with django/module 9 assignment/config/settings.py
+++ milestone 1 getting started with django/module 9 assignment/config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-(zbe5)vnk$)nbe-hk_+bqc&z_3#g(n8o@bn@&u3_&wc4fd@tm6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 # Application definition
 
