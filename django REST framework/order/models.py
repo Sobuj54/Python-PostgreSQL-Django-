038f440579fcf6f9c14831a6a221f1c67fd3e3ab
sobuj54 <sobujahmed124@gmail.com>
2025-07-28 22:11:21 +0600

    created models for phimart project

---
+++ django REST framework/order/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import User
+from product.models import Product
+
+# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"cart user {self.user.first_name}"
+    
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} -> {self.product.name}"
